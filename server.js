28fb999456f88535e098ae67902584302d7e3a52
Shruti Wagle <43674888+shrutz510@users.noreply.github.com>
2025-02-23 19:55:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const apiRoutes = require('./api');
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
